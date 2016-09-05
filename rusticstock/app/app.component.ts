20ec13358505529e92f1c728c8585c82da5558d2
Rodrigo Espinel <riespinelv@unal.edu.co>
2016-09-04 22:43:07 -0500

    Bugs npm fixed home page v1

---
+++ rusticstock/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template:
+  `
+  <div>
+    <home-comp></home-comp>
+  </div>
+  `
+})
+export class AppComponent { }
---
+++ rusticstock/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-comp',
+  templateUrl: 'app/home/home.html',
+  styleUrls: ['app/home/home.css']
+})
+export class HomeComponent {
+
+  constructor(){}
+}
