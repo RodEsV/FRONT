d7ea520ef55220ce118bfab08aad974150c8e414
Rodrigo Espinel <riespinelv@unal.edu.co>
2016-10-07 02:24:08 -0500

    setup home finishedgit branch!

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,130 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ViewEncapsulation,
+  OnInit
+} from '@angular/core';
+
+import { 
+  FormBuilder,
+  AbstractControl,
+  FormGroup, 
+  Validators,
+  FormControl,
+  FormArray
+} from '@angular/forms';
+
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService],
+  encapsulation:ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private loginService: LoginService, private fb: FormBuilder){}
+  @ViewChild('modal')
+    modal: ModalComponent;
+  loginForm: FormGroup;
+  signUpForm: FormGroup;
 
-  ngOnInit() {
+  ngOnInit(){
+    this.loginForm = this.fb.group({
+      email: [""],
+      password: [""]
+    });
+    this.signUpForm = this.fb.group({
+      name: [""],
+      nickname: [""],
+      email: [""],
+      password: [""],
+      password_confirmation: [""]
+    })
+  }
+
+  active: boolean = true;
+  errorMessage: string;
+  submitted: boolean = true;
+
+  responseLogout: boolean = true;
+  responseLogIn: any;
+  responseSignUp: any;
+
+  open(){
+    this.modal.open();
+  }
+
+  close(){
+    if(this.responseLogIn || this.responseSignUp ){
+      this.modal.close();
+    }
+  }
+  
+  /*
+  resultResponse;
+  loginUser(){
+    function exec(){
+        var auxVar;
+        console.log("executing exec");
+        this.loginService.loginUser(JSON.stringify(this.loginForm.value))
+        .subscribe(
+          response => {this.responseLogIn = response; auxVar = response},
+          error => this.errorMessage = <any>error)
+    
+        if(this.responseLogIn){
+          this.modal.close();
+        }
+        return auxVar;
+      }
+      var a = exec();
+      this.resultResponse = a;
+  }
+  */
+  resultResponse:any;
+  assignData(data:any){ 
+    this.resultResponse = data;  
+  }
+
+  
+  loginUser(){
+    this.loginService.loginUser(JSON.stringify(this.loginForm.value))
+    .subscribe(
+      response => this.responseLogIn = response,
+      error => this.errorMessage = error,
+      ()=> {this.close(); this.assignData(this.responseLogIn);}
+      );
+    return this.responseLogIn;
+  }
+  
+  // La variable resultResponse tiene el objeto Response pero hay algo raro en los headers
+  logOutUser(){
+    console.log("executing ", this.resultResponse); 
+    let infoJSON = this.resultResponse.json().data;
+    this.loginService.logOutUser( infoJSON.auth_token , infoJSON.data.email, this.responseLogIn.headers['Client'] )
+  }
+
+  signUpUser(){
+    if(this.signUpForm.controls['name'].value == ""){
+      this.signUpForm.controls['name'].setValue("default");
+    } 
+    if(this.signUpForm.controls['nickname'].value == "") {
+      this.signUpForm.controls['nickname'].setValue("default");
+    }
+    this.loginService.signUpUser(JSON.stringify(this.signUpForm.value))
+    .subscribe(
+      response => this.responseSignUp = response,
+      error => this.errorMessage = error
+      );
+  }
+
+
+
+  onSubmit(){
+    this.submitted = true;
   }
 
 }
---
+++ src/app/home/login/login.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import {
+  Http,
+  Headers,
+  Response,
+  RequestOptions
+} from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/throw';
+
+// Operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+
+
+@Injectable()
+export class LoginService{
+  constructor(private http: Http){
+  }
+  status: boolean = false;
+  /*Endpoint Rails*/
+  private logInUrl = 'http://rusticstock.herokuapp.com/api/v1/auth/sign_in.json';
+  private signUpUrl = 'http://rusticstock.herokuapp.com/api/v1/auth';
+  private logOutUrl = "http://locanhost:3000/auth/sign_out.json";
+
+  public responseLogIn;
+
+  private extractDataJSON( res: Response ){
+    let body = res.json();
+    return body.data || {};
+  }
+
+  private extractData(res: Response){
+    return res;
+  }
+  
+  loginUser( body: string){
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+    return this.http.post(this.logInUrl, body, options)
+    .map(this.extractData)
+    .catch(this.handleError);
+  }
+
+  logOutUser(access_token:any, uid:any, client: any){
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json' );
+    headers.append('access_token', access_token );
+    headers.append('uid',uid);
+    headers.append('client',client);
+    let options = new RequestOptions({headers:headers});
+    return this.http.delete(this.logOutUrl, options)
+    .map(this.extractDataJSON)
+    .catch(this.handleError);
+    
+  }
+
+  signUpUser(body: string){
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers:headers});
+    return this.http.post(this.signUpUrl,body,options)
+    .map(this.extractDataJSON)
+    .catch(this.handleError);
+  }
+
+
+  private handleError( error: any )
+  {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+}
