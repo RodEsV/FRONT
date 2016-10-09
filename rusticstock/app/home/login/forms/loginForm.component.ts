faac34ea5e88df229b421794108449f560290523
dzalduar <dzalduar@unal.edu.co>
2016-10-08 19:22:11 -0500

    deleted rusticstock folder

--- rusticstock/app/home/login/forms/loginForm.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'login-form',
-	template: ``	,
-	styles: [``]
-})
-export class LoginForm  {
-}
--- rusticstock/app/main.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './root/module/app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
