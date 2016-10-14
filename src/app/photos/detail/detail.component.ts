dad8a64c563e12ba4be8bde8b6371e7a1851ff65
Rodrigo Espinel <riespinelv@unal.edu.co>
2016-10-13 23:55:07 -0500

    end of the day detail image in progress

--- src/app/photos/detail/detail.component.ts
+++ src/app/photos/detail/detail.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+	Router,
+	ActivatedRoute,
+	Params
+	} from '@angular/router';
+import { DetailService } from "./detail.service";
+
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
+  providers: [DetailService]
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private detailService: DetailService) { }
+  image: string;
 
   ngOnInit() {
+  	this.route.params.forEach((params: Params) =>{
+  		//let id = +params['id']
+  		this.detailService.getImage(params['id']).then(photo => this.image = photo );
+  	})
   }
+
 
 }
